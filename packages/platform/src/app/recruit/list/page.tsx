bcb39446b44f80b8804ecf08bcb9bd500e36ef7b
qianqian <qianqian@mandarinhsk.com>
2024-09-18 18:47:09 +0800

    bugfix:build error

---
+++ packages/platform/src/app/recruit/list/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { observer } from 'mobx-react-lite'
+
+const RecuritList = observer(() => {
+  return (
+    <div>
+      <h1>RecruitList</h1>
+    </div>
+  )
+})
+export default RecuritList
